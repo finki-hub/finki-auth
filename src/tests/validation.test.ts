60bfd58c00de5dd4e9136f50a20e31c601557260
Delemangi <milev.stefan@gmail.com>
2025-10-07 21:27:49 +0200

    test: refactor tests

--- src/tests/validation.test.ts
+++ src/tests/validation.test.ts
@@ -5,84 +5,33 @@ import { Service } from '../lib/Service.js';
 import { isCookieValid } from '../session.js';
 import { getCookieHeader, getCredentials } from './utils.js';
 
+const testServices = [
+  { name: 'Courses', service: Service.COURSES },
+  { name: 'Diplomas', service: Service.DIPLOMAS },
+  { name: 'Old Courses', service: Service.OLD_COURSES },
+  { name: 'Masters', service: Service.MASTERS },
+];
+
 describe('Validation', () => {
-  it('should validate Courses cookies', async () => {
-    const { password, username } = getCredentials();
+  describe.each(testServices)('$name cookies', ({ service }) => {
+    it('should validate cookies', async () => {
+      const { password, username } = getCredentials();
 
-    const auth = new CasAuthentication(username, password);
-    const cookies = await auth.authenticate(Service.COURSES);
-    const headerCookies = getCookieHeader(cookies);
+      const auth = new CasAuthentication(username, password);
+      const cookies = await auth.authenticate(service);
+      const headerCookies = getCookieHeader(cookies);
 
-    const isValid = await isCookieValid(Service.COURSES, headerCookies);
+      const isValid = await isCookieValid(service, headerCookies);
 
-    expect(isValid).toBe(true);
-  });
+      expect(isValid).toBe(true);
+    });
 
-  it('should invalidate Courses cookies', async () => {
-    const headerCookies = '';
+    it('should invalidate empty cookies', async () => {
+      const headerCookies = '';
 
-    const isValid = await isCookieValid(Service.COURSES, headerCookies);
+      const isValid = await isCookieValid(service, headerCookies);
 
-    expect(isValid).toBe(false);
-  });
-
-  it('should validate Diplomas cookies', async () => {
-    const { password, username } = getCredentials();
-
-    const auth = new CasAuthentication(username, password);
-    const cookies = await auth.authenticate(Service.DIPLOMAS);
-    const headerCookies = getCookieHeader(cookies);
-
-    const isValid = await isCookieValid(Service.DIPLOMAS, headerCookies);
-
-    expect(isValid).toBe(true);
-  });
-
-  it('should invalidate Diplomas cookies', async () => {
-    const headerCookies = '';
-
-    const isValid = await isCookieValid(Service.DIPLOMAS, headerCookies);
-
-    expect(isValid).toBe(false);
-  });
-
-  it('should validate Old Courses cookies', async () => {
-    const { password, username } = getCredentials();
-
-    const auth = new CasAuthentication(username, password);
-    const cookies = await auth.authenticate(Service.OLD_COURSES);
-    const headerCookies = getCookieHeader(cookies);
-
-    const isValid = await isCookieValid(Service.OLD_COURSES, headerCookies);
-
-    expect(isValid).toBe(true);
-  });
-
-  it('should invalidate Old Courses cookies', async () => {
-    const headerCookies = '';
-
-    const isValid = await isCookieValid(Service.OLD_COURSES, headerCookies);
-
-    expect(isValid).toBe(false);
-  });
-
-  it('should validate Masters cookies', async () => {
-    const { password, username } = getCredentials();
-
-    const auth = new CasAuthentication(username, password);
-    const cookies = await auth.authenticate(Service.MASTERS);
-    const headerCookies = getCookieHeader(cookies);
-
-    const isValid = await isCookieValid(Service.MASTERS, headerCookies);
-
-    expect(isValid).toBe(true);
-  });
-
-  it('should invalidate Masters cookies', async () => {
-    const headerCookies = '';
-
-    const isValid = await isCookieValid(Service.MASTERS, headerCookies);
-
-    expect(isValid).toBe(false);
+      expect(isValid).toBe(false);
+    });
   });
 });
