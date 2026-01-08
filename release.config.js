8f3e4fb8782989f788e1df3b1aa60beadf387c51
Delemangi <milev.stefan@gmail.com>
2026-01-08 04:52:00 +0100

    fix(releasing): simplify releasing workflow

--- release.config.js
+++ release.config.js
@@ -6,19 +6,11 @@ export default {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
     "@semantic-release/npm",
     [
       "@semantic-release/github",
       {
         successCommentCondition: false
-      }
-    ],
-    [
-      "@semantic-release/git",
-      {
-        assets: ["package.json", "package-lock.json", "CHANGELOG.md"],
-        message: "chore(release): ${nextRelease.version}"
       }
     ]
   ],
